8955038ff01625ab79c0a8f7ef381eb0f9ac5eb5
GPS <48364069+gpsgit@users.noreply.github.com>
2022-04-02 08:22:13 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -7,5 +7,11 @@ terraform {
   }
 
   required_version = ">= 0.14"
+  
+  backend "azurerm" {
+    resource_group_name  = "tfacctstorage-rg"
+    storage_account_name = "tfacctstorage"
+    container_name       = "tfstate"
+    key                  = "key"
+  }
 }
-
